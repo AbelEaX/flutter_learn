e90a9eb82bb3a2f3f618fde1e9a48a57e7b070bb
Abel Ekode <abelekode@gmail.com>
2024-08-01 06:20:54 +0300

    new update

--- lib/prac4.dart
+++ lib/prac4.dart
@@ -8,36 +8,399 @@ class Prac4 extends StatefulWidget {
 }
 
 class _Prac4State extends State<Prac4> {
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 1,
+          toolbarHeight: 1, backgroundColor: Color.fromARGB(255, 27, 27, 27)),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Color.fromARGB(255, 27, 27, 27)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.sports_soccer_outlined),
+              label: 'Games',
+              backgroundColor: Color.fromARGB(255, 27, 27, 27)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.groups),
+              label: 'Groups',
+              backgroundColor: Color.fromARGB(255, 27, 27, 27)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_rounded),
+              label: 'Profile',
+              backgroundColor: Color.fromARGB(255, 27, 27, 27)),
+        ],
       ),
-      body: Stack(
+      body: Column(
         children: [
-          Column(
+          Stack(
             children: [
               Container(
-                height: 10,
+                height: 782.5,
                 width: double.infinity,
+                child: Image.asset(
+                  'assets/pl2.jpg',
+                  fit: BoxFit.cover,
+                ),
               ),
               Positioned(
-                top: 20,
-                left: 25,
-                child: Image.asset('assets/pl2.jpg'),
-              ),
-              Positioned(
-                top: 20,
-                left: 25,
                 child: Container(
-                  height: 400,
-                  width: 00,
-                  color: Colors.black.withOpacity(0.8),
+                  margin: EdgeInsets.all(15),
+                  height: 750,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.7),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20.0, top: 10),
+                            child: Icon(
+                              Icons.cancel_rounded,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 20.0, top: 10),
+                            child: Container(
+                              height: 40,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Color.fromARGB(255, 255, 215, 105)),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Done',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  Icon(Icons.check),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Text(
+                        'ORGANISE YOUR GAME',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0, right: 20),
+                        child: Container(
+                          height: 1,
+                          width: 400,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 10),
+                        child: Container(
+                          height: 30,
+                          width: 180,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Color.fromARGB(255, 150, 37, 226)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'BREXIT GROUP',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            right: 20.0, left: 20, top: 20),
+                        child: Container(
+                          height: 60,
+                          width: 400,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8),
+                            //color: Color.fromARGB(255, 150, 37, 226),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'Westminster Abbey, london,..',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 25.0, left: 40),
+                                child: Text(
+                                  'START DATE',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    right: 0, top: 10, left: 40),
+                                child: Container(
+                                  height: 30,
+                                  width: 135,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        '08 Nov 19 - 12:30',
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w700,
+                                            color: const Color.fromARGB(
+                                                255, 0, 0, 0)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 25.0,
+                                ),
+                                child: Text(
+                                  'END DATE',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    right: 20.0, top: 10, left: 30),
+                                child: Container(
+                                  height: 30,
+                                  width: 135,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        '08 Nov 19 - 13:30',
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w700,
+                                            color: const Color.fromARGB(
+                                                255, 0, 0, 0)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 65.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Game Mode',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromARGB(255, 255, 255, 255)),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '7-A-Side',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromARGB(255, 255, 145, 55)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 60.0),
+                        child: Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  'RESERVES',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                ),
+                                Text(
+                                  '2',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      color:
+                                          Color.fromARGB(255, 255, 255, 255)),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 40,
+                            ),
+                            Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 25.0),
+                                  child: Text(
+                                    'COST PER GAME',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255)),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '\£',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.w700,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255)),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Container(
+                                      height: 40,
+                                      width: 120,
+                                      child: Text(
+                                        '100',
+                                        style: TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.w700,
+                                            color: Color.fromARGB(
+                                                255, 255, 255, 255)),
+                                      ),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Cost per player:',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.w700,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255)),
+                                    ),
+                                    Text(
+                                      '\£',
+                                      style: TextStyle(
+                                          fontSize: 25,
+                                          fontWeight: FontWeight.w700,
+                                          color: Color.fromARGB(
+                                              255, 255, 255, 255)),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
+          Container()
         ],
       ),
     );
